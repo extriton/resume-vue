c8c8c4e2deb366cc9b7a44cbcc57219494ce5bbe
unknown <you@example.com>
2019-09-24 20:02:16 +0300

    Created

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path')
 const serveStatic = require('serve-static')
 const app = express()
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use('/*', res.render(index))
 const port = process.env.PORT || 8080
 app.listen(port)
 console.log('server started ' + port)
